0014cd436ea8fabec67b62ef9823f0ba6099bce2
Brian Nguyen <bnguyen16@Brians-MacBook-Pro-2.local>
2018-03-20 22:48:37 -0500

    added logic for login

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -9,18 +9,47 @@ var config = {
 };
 firebase.initializeApp(config);
 
+//////////////////////////////////////////
+// Authentication //
+/////////////////////////////////////////
 
-var provider = new firebase.auth.GoogleAuthProvider();
-var provider = new firebase.auth.FacebookAuthProvider();
-var provider = new firebase.auth.TwitterAuthProvider();
-var provider = new firebase.auth.GithubAuthProvider();
+//Login elements
+var userName = document.getElementById("someUsername");
+var userPassword = document.getElementById("somePassword");
+var btnLogin = document.getElementById("someLogin");
+var btnSignup = document.getElementById("someSignup");
+var btnSignout = document.getElementById("someSignout");
 
-auth.currentUser.linkWithPopup(provider).then(function (result) {
-    // Accounts successfully linked.
-    var credential = result.credential;
-    var user = result.user;
-    // ...
-}).catch(function (error) {
-    // Handle Errors here.
-    // ...
+//Login event listener
+btnLogin.addEventListener('click', e => {
+//Collect login info
+    var email = userName.value;
+    var pass = userPassword.value;
+    var auth = firebase.auth();
+
+//Log in
+    var promise = auth.signInWithEmailAndPassword(email, pass);
+    promise.catch(e => console(e.message));
+});
+
+//Sign up event listener
+btnSignup.addEventListener('click', e => {
+//Collect login info
+    var email = userName.value;
+    var pass = userPassword.value;
+    var auth = firebase.auth();
+
+//Sign up
+    var promise = auth.createUserWithEmailAndPassword(email, pass);
+    promise
+        .catch(e => console(e.message));
+});
+
+//Add listener 
+firebase.auth().onAuthStateChanged(firebaseUser => {
+    if(firebaseUser) {
+        console.log(firebaseUser);
+    } else {
+        console.log("not logged in");
+    }
 });
