00d2090cb8db66718c635f1d54f2a0782689f735
Shamil Arefin <landoflitany@gmail.com>
2018-03-20 21:16:30 -0500

    uWu New First

--- assets/java/weather.js
+++ assets/java/weather.js
@@ -1,15 +1,45 @@
-function displayWeather(){
-$("#submit-location").on("click", function(){
-    var apiKey = "c8db54c823c2bfec89a9f0724d34413f";
-    var location = $("#inputLocation");
-    var queryURL = "api.openweathermap.org/data/2.5/forecast?zip=" + location + "&appid=" + apiKey;
+var movies = ["The Matrix", "The Notebook", "Mr. Nobody", "The Lion King"];
+function displayMovieInfo() {
+    var apiKey = "b5a04afb";
+    var movie = $(this).attr("data-name").val().trim();
+    var queryURL = "https://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=" + apiKey;
+    console.log(queryURL);
 
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-      }).then(function(response) {
-          var results = response.data;
-          console.log(results);
-      })
-});
-}
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function (response) {
+            var movieDiv = $("<div class='movie'>");
+            var rating = response.Rated;
+            var pOne = $("<p>").text("Rating: " + rating);
+            movieDiv.append(pOne);
+            var released = response.Released;
+            var pTwo = $("<p>").text("Released: " + released);
+            movieDiv.append(pTwo);
+            var plot = response.Plot;
+            var pThree = $("<p>").text("Plot: " + plot);
+            movieDiv.append(pThree);
+            var imgURL = response.Poster;
+            var image = $("<img>").attr("src", imgURL);
+            movieDiv.append(image);
+            $("#movies-view").prepend(movieDiv);
+    });
+    function renderButtons() {
+            $("#buttons-view").empty();
+            for (var i = 0; i < movies.length; i++) {
+                var a = $("<button>");
+                a.addClass("movie-btn");
+                a.attr("data-name", movies[i]);
+                a.text(movies[i]);
+                $("#buttons-view").append(a);
+            }
+        };
+    $("#add-movie").on("click", function (event) {
+            event.preventDefault();
+            var movie = $("#movie-input").val().trim();
+            movies.push(movie);
+            renderButtons();
+        });
+    $(document).on("click", ".movie-btn", displayMovieInfo);
+    renderButtons();
+    }
